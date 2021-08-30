93c6703e31024649c4655d8f058b7d3f943f8d37
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2021-08-29 18:40:30 -0700

    Added backup storage system.

--- filewatcherstorage.py
+++ filewatcherstorage.py
@@ -2,6 +2,7 @@ import time
 import requests
 import hashlib
 import os
+import pickle
 
 
 class WatchedFile:
@@ -20,12 +21,15 @@ class WatchedFile:
         self.local_path = ''
 
     def download(self):
+        print(f'Download: GET {self.url}...')
         r = requests.get(self.url)
+        print(f'Done...')
         self.last_checked = time.time()
         self.last_status = r.status_code
         if r.status_code != 200:
             print(f'WatchedFile.download: Error; status code is {r.status_code} not 200; cancelling download')
             return
+        print(f'Status 200 OK')
         print(f'WatchedFile.download: File recieved, hashing')
         this_sum = hashlib.sha256(r.content).hexdigest()
         print(f'WatchedFile.download: Hash is {this_sum[:16]}... (comparing to {self.last_sum[:16]})')
@@ -39,14 +43,23 @@ class WatchedFile:
             print(f'Saved')
 
 
+def pickle_file(wf: WatchedFile):
+    print(f'pickle_file: writing to reload_files/{wf.folder_name}.dat')
+    with open(f'reload_files/{wf.folder_name}.dat', 'wb') as f:
+        pickle.dump(wf, f)
+
+
 class FileWatchingGuild:
     def __init__(self, guildid: int):
         self.id = guildid
         self.watching = []
-        
+
+
 if __name__ == '__main__':
     print('TESTING MODE')
     f = WatchedFile('https://www.firstinspires.org/sites/default/files/uploads/resource_library/ftc/game-manual-part-1-traditional-events.pdf')
     f2 = WatchedFile('https://www.firstinspires.org/sites/default/files/uploads/resource_library/ftc/game-manual-part-1-remote-events.pdf')
     f.download()
     f2.download()
+    pickle_file(f)
+    pickle_file(f2)
